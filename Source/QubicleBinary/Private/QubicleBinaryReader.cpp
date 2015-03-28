9a2d9c89bc62453fba5281139a161f061dec9fc5
afuzzyllama <karcieri@gmail.com>
2015-03-28 16:06:50 -0400

    Binary reader is working

--- Source/QubicleBinary/Private/QubicleBinaryReader.cpp
+++ Source/QubicleBinary/Private/QubicleBinaryReader.cpp
@@ -1,219 +1,242 @@
 // Copyright 2015 afuzzyllama. All Rights Reserved.
 
 #include "QubicleBinaryPrivatePCH.h"
+#include <string>	// Convert std::string to FString
 #include "QubicleBinaryReader.h"
+
 
 const uint32 FQubicleBinaryReader::CODEFLAG = 2;
 const uint32 FQubicleBinaryReader::NEXTSLICEFLAG = 6;
 
 bool FQubicleBinaryReader::ReadFileIntoObject(FString FileName, IQubicleBinaryAccessor* ContainerObject)
 {
-    check(ContainerObject != nullptr);
-    
-    TArray<uint8> BinaryData;
-    if(!FFileHelper::LoadFileToArray(BinaryData, *FileName))
-    {
-        UE_LOG(LogQubicleBinary, Error, TEXT("Could not read file"));
-    }
-    
-    return FQubicleBinaryReader::ReadIntoObject(BinaryData, ContainerObject);
-    
+	check(ContainerObject != nullptr);
+
+	TArray<uint8> BinaryData;
+	if (!FFileHelper::LoadFileToArray(BinaryData, *FileName))
+	{
+		UE_LOG(LogQubicleBinary, Error, TEXT("Could not read file"));
+	}
+
+	return FQubicleBinaryReader::ReadIntoObject(BinaryData, ContainerObject);
+
 }
 
 bool FQubicleBinaryReader::ReadIntoObject(TArray<uint8>& BinaryData, IQubicleBinaryAccessor* ContainerObject)
 {
-    check(ContainerObject != nullptr);
+	check(ContainerObject != nullptr);
 
-    FMemoryReader Reader(BinaryData, false);
-    Reader.Seek(0);
-    
-    uint8 Major, Minor, Release, Build;
-    Reader << Major;
-    Reader << Minor;
-    Reader << Release;
-    Reader << Build;
+	FMemoryReader Reader(BinaryData, false);
+	Reader.Seek(0);
 
-    UE_LOG(LogQubicleBinary, Verbose, TEXT("Version: %i.%i.%i.%i"), Major, Minor, Release, Build);
-    
-    // Version selection
-    switch(Major)
-    {
-    case 1:
-        switch (Minor)
-        {
-        case 1:
-            switch (Release)
-            {
-            case 0:
-                switch (Build)
-                {
-                case 0:
-                    return FQubicleBinaryReader::ReadIntoObject_1_1_0_0(BinaryData, ContainerObject);
-                    break;
+	uint8 Major, Minor, Release, Build;
+	Reader << Major;
+	Reader << Minor;
+	Reader << Release;
+	Reader << Build;
 
-                }
-                break;
-            }
-        }
-    }
-    
-    // error
-    UE_LOG(LogQubicleBinary, Error, TEXT("Version not implemented: %i.%i.%i.%i"), Major, Minor, Release, Build);
-    return false;
+	UE_LOG(LogQubicleBinary, Verbose, TEXT("Version: %i.%i.%i.%i"), Major, Minor, Release, Build);
+
+	// Version selection
+	switch (Major)
+	{
+	case 1:
+		switch (Minor)
+		{
+		case 1:
+			switch (Release)
+			{
+			case 0:
+				switch (Build)
+				{
+				case 0:
+					return FQubicleBinaryReader::ReadIntoObject_1_1_0_0(BinaryData, ContainerObject);
+					break;
+				}
+				break;
+			}
+		}
+	}
+
+	// error
+	UE_LOG(LogQubicleBinary, Error, TEXT("Version not implemented: %i.%i.%i.%i"), Major, Minor, Release, Build);
+	return false;
 }
 
 bool FQubicleBinaryReader::ReadIntoObject_1_1_0_0(TArray<uint8>& BinaryData, IQubicleBinaryAccessor* ContainerObject)
 {
-    FMemoryReader Reader(BinaryData, false);
+	FMemoryReader Reader(BinaryData, false);
 
-    // Skip reading the version which is uint32
-    Reader.Seek(4);
-    
-    uint32 ColorFormat; // 0 = RGBA
-                        // 1 = BGRA
+	// Skip reading the version which is uint32
+	Reader.Seek(4);
 
-    uint32 ZAxisOrientation; // 0 = left handed
-                             // 1 = right handed
+	uint32 ColorFormat; // 0 = RGBA
+	// 1 = BGRA
 
-    uint32 Compressed; // 0 = uncompressed
-                       // 1 = compressed using run length encoding (RLE)
+	uint32 ZAxisOrientation; // 0 = left handed
+	// 1 = right handed
 
-    uint32 VisibilityMaskEncoded;   // 0 = the A value of RGBA or BGRA is either 0 (invisble voxel) or 255 (visible voxel).
-                                    // 1 = the visibility mask of each voxel is encoded into the A value telling your software which sides of the voxel are visible. You can save a lot of render time using this option. More info about this in the section visibility-mask encoding.
-                                    // if (mask == 0) // voxel invisble
-                                    // if (mask && 2 == 2) // left side visible
-                                    // if (mask && 4 == 4) // right side visible
-                                    // if (mask && 8 == 8) // top side visible
-                                    // if (mask && 16 == 16) // bottom side visible
-                                    // if (mask && 32 == 32) // front side visible
-                                    // if (mask && 64 == 64) // back side visible
+	uint32 Compressed; // 0 = uncompressed
+	// 1 = compressed using run length encoding (RLE)
 
-    uint32 NumMatrices;
-    
-    Reader << ColorFormat;
-    Reader << ZAxisOrientation;
-    Reader << Compressed;
-    Reader << VisibilityMaskEncoded;
-    Reader << NumMatrices;
- 
-    // for each matrix stored in file
-    for (int32 i = 0; i < NumMatrices; ++i)
-    {
-        // read matrix name
-        uint8 MatrixNameLength;
-        FString MatrixName;
-        
-        // Read the length
-        Reader << MatrixNameLength;
-        
-        // Get a pointer to the position in the BinaryData array that points to the beginning on the string.
-        // Read the string into the variable
-        MatrixName = FString::FromBlob(BinaryData.GetData() + Reader.Tell(), MatrixNameLength);
-        
-        // Seek the reader to after the string position
-        Reader.Seek(Reader.Tell() + MatrixNameLength);
-        
- 	    // read matrix size
-        uint32 SizeX, SizeY, SizeZ;
-        Reader << SizeX;
-        Reader << SizeY;
-        Reader << SizeZ;
-  
-        // read matrix position (in this example the position is irrelevant)
-        int32 PosX, PosY, PosZ;
-        Reader << PosX;
-        Reader << PosY;
-        Reader << PosZ;
-        
-        ContainerObject->InitContainer(MatrixName, PosX, PosY, PosZ, SizeX, SizeY, SizeZ);
-        
-        if (Compressed == 0)
-        {
-            // if uncompressd
-            for(uint32 z = 0; z < SizeZ; ++z)
-            {
-                for(uint32 y = 0; y < SizeY; y++)
-                {
-                    for(uint32 x = 0; x < SizeX; x++)
-                    {
-                        ContainerObject->SetVoxel(MatrixName, x, y, z, FQubicleBinaryReader::ReadColor(&Reader, ColorFormat));
-                    }
-                }
-            }
-        }
-        else
-        {
-            // if compressed
-            
-            uint32 Index;
-            for(uint32 z = 0; z < SizeZ; ++z)
-            {
-                z++;
-                Index = 0;
-       
-                uint32 Data;
-                uint32 Count;
-                while (true)
-                {
-                    Reader << Data;
-         
-                    uint32 x, y;
-                    if (Data == FQubicleBinaryReader::NEXTSLICEFLAG)
-                    {
-                        break;
-                    }
-                    else if (Data == FQubicleBinaryReader::CODEFLAG)
-                    {
-                        Reader << Count;
-                        Reader << Data;
+	uint32 VisibilityMaskEncoded;   // 0 = the A value of RGBA or BGRA is either 0 (invisble voxel) or 255 (visible voxel).
+	// 1 = the visibility mask of each voxel is encoded into the A value telling your software which sides of the voxel are visible. You can save a lot of render time using this option. More info about this in the section visibility-mask encoding.
+	// if (mask == 0) // voxel invisble
+	// if (mask && 2 == 2) // left side visible
+	// if (mask && 4 == 4) // right side visible
+	// if (mask && 8 == 8) // top side visible
+	// if (mask && 16 == 16) // bottom side visible
+	// if (mask && 32 == 32) // front side visible
+	// if (mask && 64 == 64) // back side visible
+
+	uint32 NumMatrices;
+
+	Reader << ColorFormat;
+	Reader << ZAxisOrientation;
+	Reader << Compressed;
+	Reader << VisibilityMaskEncoded;
+	Reader << NumMatrices;
+
+	// for each matrix stored in file
+	for (uint32 i = 0; i < NumMatrices; ++i)
+	{
+		// read matrix name
+		uint8 MatrixNameLength;
+
+		// Read the length
+		Reader << MatrixNameLength;
+
+		// Get a pointer to the position in the BinaryData array that points to the beginning on the string.
+		// Read the string into the variable
+		FString MatrixName("");
+		for (uint8 CharNum = 0; CharNum < MatrixNameLength; ++CharNum)
+		{
+			uint8 CurrentByte;
+			Reader << CurrentByte;
+
+			FString CurrentChar = FString::Printf(TEXT("%c"), CurrentByte);
+			MatrixName += CurrentChar;
+		}
 
 
-                        for(uint32 j = 0; j < Count; ++j)
-                        {
-                            x = Index % SizeX + 1; // mod = modulo e.g. 12 mod 8 = 4
-                            y = Index / SizeX + 1; // div = integer division e.g. 12 div 8 = 1
-                            Index++;
+		// read matrix size
+		uint32 SizeX, SizeY, SizeZ;
+		Reader << SizeX;
+		Reader << SizeY;
+		Reader << SizeZ;
 
-                            ContainerObject->SetVoxel(MatrixName, x, y, z, FQubicleBinaryReader::ReadColor(&Reader, ColorFormat));
-                        }
-                    }
-                    else
-                    {
-                        x = Index % SizeX + 1;
-                        y = Index / SizeX + 1;
-                        Index++;
+		// read matrix position (in this example the position is irrelevant)
+		int32 PosX, PosY, PosZ;
+		Reader << PosX;
+		Reader << PosY;
+		Reader << PosZ;
 
-                        ContainerObject->SetVoxel(MatrixName, x, y, z, FQubicleBinaryReader::ReadColor(&Reader, ColorFormat));
-                    }
-                }
-            }
-        }
-    }
-    
-    return true;
+		ContainerObject->InitContainer(MatrixName, PosX, PosY, PosZ, SizeX, SizeZ, SizeY);
+
+		uint32 Data;
+		FColor NewColor;
+		if (Compressed == 0)
+		{
+			// if uncompressd
+			for (uint32 z = 0; z < SizeZ; ++z)
+			{
+				for (uint32 y = 0; y < SizeY; y++)
+				{
+					for (uint32 x = 0; x < SizeX; x++)
+					{
+						Reader << Data;
+
+						NewColor = FQubicleBinaryReader::ReadColor(Data, ColorFormat);
+						if (NewColor.A != 0)
+						{
+							ContainerObject->SetVoxel(MatrixName, x, z, y, NewColor);
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			// if compressed
+
+			uint32 Index;
+			for (uint32 z = 0; z < SizeZ; ++z)
+			{
+				Index = 0;
+
+				uint32 Count;
+				while (true)
+				{
+					Reader << Data;
+
+					uint32 x, y;
+					if (Data == FQubicleBinaryReader::NEXTSLICEFLAG)
+					{
+						break;
+					}
+					else if (Data == FQubicleBinaryReader::CODEFLAG)
+					{
+						Reader << Count;
+						Reader << Data;
+
+
+						for (uint32 j = 0; j < Count; ++j)
+						{
+							x = Index % SizeX; // mod = modulo e.g. 12 mod 8 = 4
+							y = Index / SizeX; // div = integer division e.g. 12 div 8 = 1
+							++Index;
+
+							NewColor = FQubicleBinaryReader::ReadColor(Data, ColorFormat);
+							if (NewColor.A != 0)
+							{
+								ContainerObject->SetVoxel(MatrixName, x, z, y, NewColor);
+							}
+
+						}
+					}
+					else
+					{
+						x = Index % SizeX;
+						y = Index / SizeX;
+						++Index;
+
+						NewColor = FQubicleBinaryReader::ReadColor(Data, ColorFormat);
+						if (NewColor.A != 0)
+						{
+							ContainerObject->SetVoxel(MatrixName, x, z, y, NewColor);
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+	return true;
 }
 
 
-FColor FQubicleBinaryReader::ReadColor(FMemoryReader* Reader, uint32 ColorFormat)
+
+FColor FQubicleBinaryReader::ReadColor(uint32 Color, uint32 ColorFormat)
 {
     uint8 R,G,B,A;
-    if(ColorFormat == 0)
-    {
-        // RGBA
-        (*Reader) << R;
-        (*Reader) << G;
-        (*Reader) << B;
-        (*Reader) << A;
-    }
-    else
-    {
-        // BGRA
-        (*Reader) << B;
-        (*Reader) << G;
-        (*Reader) << R;
-        (*Reader) << A;
-    }
+	if (ColorFormat == 0)
+	{
+		// RGBA
+		A = (Color & 0xFF000000) >> 24;
+		B = (Color & 0x00FF0000) >> 16;
+		G = (Color & 0x0000FF00) >> 8;
+		R = (Color & 0x000000FF);
+	}
+	else
+	{
+		// BGRA
+		A = (Color & 0xFF000000) >> 24;
+		R = (Color & 0x00FF0000) >> 16;
+		G = (Color & 0x0000FF00) >> 8;
+		B = (Color & 0x000000FF);
+	}
     
     // Ignoring A component because masking is done outside of loading
-    return FColor(R,G,B);
+    return FColor(R,G,B,A);
 }
--- Source/QubicleBinary/Public/QubicleBinaryReader.h
+++ Source/QubicleBinary/Public/QubicleBinaryReader.h
@@ -1,19 +1,36 @@
 // Copyright 2015 afuzzyllama. All Rights Reserved.
 #pragma once
 
+/**
+ * Interface to enable setting of voxels on a game asset
+ */
 class IQubicleBinaryAccessor
 {
 public:
     virtual ~IQubicleBinaryAccessor() {}
+
+	/**
+	  *	Initialize a "Matrix" from the Qubicle Editor.  Assumes that the initialized container is empty.
+	  */
     virtual void InitContainer(FString ContainerName, int32 PosX, int32 PosY, int32 PosZ, uint32 SizeX, uint32 SizeY, uint32 SizeZ) = 0;
+
+	/**
+	 *	Sets a voxel inside the supplied container 
+	 */
     virtual void SetVoxel(FString ContainerName, uint32 x, uint32 y, uint32 z, FColor Color) = 0;
 };
 
-class FQubicleBinaryReader
+class QUBICLEBINARY_API FQubicleBinaryReader
 {
 public:
+	/**
+	 *	Reads a Qubicle Binary file on to an object that implents the IQubicleBinaryAccessor interface
+	 */
     static bool ReadFileIntoObject(FString FileName, IQubicleBinaryAccessor* ContainerObject);
 
+	/**
+	 *	Reads a byte array filled with a Qubicle Binary file on to an object that implents the IQubicleBinaryAccessor interface
+	 */
     static bool ReadIntoObject(TArray<uint8>& BinaryData, IQubicleBinaryAccessor* ContainerObject);
     
 private:
@@ -21,6 +38,9 @@ private:
     static const uint32 NEXTSLICEFLAG;
 
     static bool ReadIntoObject_1_1_0_0(TArray<uint8>& BinaryData, IQubicleBinaryAccessor* ContainerObject);
-    
-    static FColor ReadColor(FMemoryReader* Reader, uint32 ColorFormat);
+
+	/**
+	 * Converts a uint32 to FColor.  Format Qubicle Editor uses is different than Unreal's FColor(uint32) constructor, so that conversion is taken care of here.
+	 */
+	static FColor FQubicleBinaryReader::ReadColor(uint32 Color, uint32 ColorFormat);
 };
